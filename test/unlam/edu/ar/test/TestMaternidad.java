e327dfd175aeb40ed5ebc8b408f3d6debf91fe89
Gonzalo Herlein <gherlein@alumno.unlam.edu.ar>
2023-05-06 16:05:36 -0300

    primer commit gonza

--- test/unlam/edu/ar/test/TestMaternidad.java
+++ test/unlam/edu/ar/test/TestMaternidad.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import unlam.edu.ar.dominio.Clinica;
+import unlam.edu.ar.dominio.Medico;
 
 public class TestMaternidad {
 
@@ -14,4 +15,9 @@ public class TestMaternidad {
 		assertNotNull(clinica);
 	}
 
+	@Test
+	public void queSePuedaCrearUnMedico() {
+		Medico medico = new Medico();
+		assertNotNull(medico);
+	}
 }
