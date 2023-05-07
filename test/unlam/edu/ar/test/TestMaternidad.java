7c324d0fc47dfc579ddb8c8ff619c8d6aadebaf9
Gonzalo Herlein <gherlein@alumno.unlam.edu.ar>
2023-05-07 11:02:11 -0300

    parte gonza terminada

--- test/unlam/edu/ar/test/TestMaternidad.java
+++ test/unlam/edu/ar/test/TestMaternidad.java
@@ -6,8 +6,8 @@ import org.junit.Test;
 
 import unlam.edu.ar.dominio.Clinica;
 import unlam.edu.ar.dominio.Medico;
-import unlam.edu.ar.dominio.Parteras;
 import unlam.edu.ar.dominio.Obstetra;
+import unlam.edu.ar.dominio.Partera;
 
 
 public class TestMaternidad {
@@ -19,39 +19,110 @@ public class TestMaternidad {
 	}
 	
 	@Test
-	public void queSePuedCrearUnaObstetra() {
-		Obstetra obstre = new Obstetra();
-		assertNotNull(obstre);
-	}
-
-	@Test
 	public void queSePuedaCrearUnMedico() {
 		Medico medico = new Medico();
 		assertNotNull(medico);
 	}
 	
 	@Test
-	public void queSePuedaCrearUnaPartera() {
-		Parteras partera = new Parteras();
-		assertNotNull(partera);
+	public void queSePuedaCrearUnaObstetra() {
+		Obstetra obstre = new Obstetra();
+		assertNotNull(obstre);
 	}
 
 	@Test
-	public void queSePuedaCrearUnaPartera123() {
-		Parteras partera123 = new Parteras();
-		assertNotNull(partera123);
+	public void queSePuedaCrearUnaParteraConUnNumeroDeTelefono() {
+		String nombrePartera = "Eduarda";
+		Integer edadPartera = 23;
+		String telefonoPartera = "1134236477";
+		Partera partera = new Partera(nombrePartera, edadPartera, "123",telefonoPartera );
+		
+		
+		assertTrue(partera.validarTelefono(telefonoPartera));
+	}
+
+	@Test
+	public void queSePuedaSaberSiElObstetraTrabajaFinDeSemana() {
+		String nombreObstetra = "Eduardo";
+		Integer edadPartera = 23;
+
+		Obstetra obstetra = new Obstetra(nombreObstetra,edadPartera,"123" ,20);
+		
+		assertTrue(obstetra.validarTrabajoFinDeSemana());
+		
 	}
 	
 	@Test
-	public void queSePuedaCrearUnaPartera1235() {
-		Parteras partera1235 = new Parteras();
-		assertNotNull(partera1235);
+	public void queSePuedaValidarUnaParteraConUnDNIValido() {
+		String nombrePartera = "Eduarda";
+		Integer edadPartera = 23;
+		String telefonoPartera = "1134236477";
+		String dni = "43861369";
+		Partera partera = new Partera(nombrePartera, edadPartera, dni,telefonoPartera);
+		
+		assertTrue(partera.validarDni(dni));
+		assertNotNull(partera);
 	}
 	
 	@Test
-	public void queSePuedaCrearUnaPartera123567() {
-		Parteras partera123567 = new Parteras();
-		assertNotNull(partera123567);
+	public void queSePuedaAgregarUnaParteraConUnDNIValidoYConUnTelefonoValido() {
+		String nombrePartera = "Eduarda";
+		Integer edadPartera = 23;
+		String telefonoPartera = "1134236477";
+		String dni = "43861369";
+		String nombreClinica = "Hospital Italiano";
+		
+		Clinica clinica = new Clinica(nombreClinica);
+		Partera partera = new Partera(nombrePartera, edadPartera, dni,telefonoPartera);
+		
+		
+		assertTrue(partera.validarTelefono(telefonoPartera));
+		assertTrue(partera.validarDni(dni));
+		clinica.agregarMedicos(partera);
+		
+		assertEquals(clinica.getListaDeMedicos().size(),  1);
+		
+	}
+	
+	@Test
+	public void queSePuedaAgregarUnaObstetraQueSoloTrabajeLosFinDeSemana() {
+		String nombreObstetra = "Eduardo";
+		Integer edadObstetra = 23;
+		String dni = "43861369";
+		String nombreClinica = "Hospital Italiano";
+		
+		Obstetra obstetra = new Obstetra(nombreObstetra,edadObstetra,dni ,20);
+		Clinica clinica = new Clinica(nombreClinica);
+
+		
+		
+		assertTrue(obstetra.validarTrabajoFinDeSemana());
+		assertTrue(obstetra.validarDni(dni));
+		clinica.agregarMedicos(obstetra);
+		
+		assertEquals(clinica.getListaDeMedicos().size(),  1);
+		
+	}
+	
+	@Test
+	public void queNoSePuedaAgregarUnaObstetraRepetido() {
+		String nombreObstetra = "Eduardo";
+		String nombreObstetra2 = "Juan";
+		Integer edadObstetra = 23;
+		String dni = "43861369";
+		String dni2 = "43861369";
+		String nombreClinica = "Hospital Italiano";
+		
+		Obstetra obstetra = new Obstetra(nombreObstetra,edadObstetra,dni ,20);
+		Obstetra obstetra2 = new Obstetra(nombreObstetra2,edadObstetra,dni2 ,20);
+		Clinica clinica = new Clinica(nombreClinica);
+
+		
+		clinica.agregarMedicos(obstetra);
+		clinica.agregarMedicos(obstetra2);
+		
+		assertEquals(clinica.getListaDeMedicos().size(),  1);
+		
 	}
 
 }
